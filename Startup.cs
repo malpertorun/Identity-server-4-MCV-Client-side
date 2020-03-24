d68c5d8c9dc46258402f105113bd82ff2f6e1304
Alper Torun <31883160+malpertorun@users.noreply.github.com>
2020-03-24 13:29:27 +0300

    .

---
+++ Startup.cs
@@ -0,0 +1,68 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace MvcClient
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = "Cookies";
+                options.DefaultChallengeScheme = "oidc";
+            })
+                .AddCookie("Cookies")
+                .AddOpenIdConnect("oidc", options =>
+                {
+                    options.Authority = "https://gappsts.azurewebsites.net";
+                    options.RequireHttpsMetadata = false;
+
+                    options.ClientId = "G T S";
+                    options.ClientSecret = "VE SECRET DEGERI";
+                    options.ResponseType = "code";
+
+                    options.SaveTokens = false;
+                    options.Scope.Add("openid");
+
+                    options.Scope.Add("profile");
+                    options.Scope.Add("email");
+                 
+
+
+                    options.Scope.Add("offline_access");
+                });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute()
+                    .RequireAuthorization();
+            });
+        }
+    }
+}
